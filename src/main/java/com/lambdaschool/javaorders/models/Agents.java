c1519f85c16d220bcfe824eff94e6a253738f421
Jacob Seppanen <jbseppanen@gmail.com>
2019-02-19 12:12:19 -0800

    Add Agent data type.

---
+++ src/main/java/com/lambdaschool/javaorders/models/Agents.java
@@ -0,0 +1,62 @@
+package com.lambdaschool.javaorders.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "agents")
+public class Agents {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String agentName, workingArea, phone, country;
+    private double commission;
+
+    public Agents() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getAgentName() {
+        return agentName;
+    }
+
+    public void setAgentName(String agentName) {
+        this.agentName = agentName;
+    }
+
+    public String getWorkingArea() {
+        return workingArea;
+    }
+
+    public void setWorkingArea(String workingArea) {
+        this.workingArea = workingArea;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public double getCommission() {
+        return commission;
+    }
+
+    public void setCommission(double commission) {
+        this.commission = commission;
+    }
+}
